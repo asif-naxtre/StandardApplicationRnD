8834a8c71faef8a6f42ddd703f3894bbe418c791
Asif Khan <asif.naxtre@gmail.com>
2020-09-10 10:40:50 +0530

    memory leak

--- app/src/main/java/com/rnd/standardapplicationrnd/ui/activity/MainActivity.kt
+++ app/src/main/java/com/rnd/standardapplicationrnd/ui/activity/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.rnd.standardapplicationrnd.ui.activity
 
+import android.content.res.Configuration
+import android.os.SystemClock
+import androidx.appcompat.app.AppCompatActivity
 import com.rnd.standardapplicationrnd.R
 
-class MainActivity : BaseActivity() {
 
-    override fun getLayout(): Int = R.layout.activity_main
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    override fun initialSetUp() {
+//    override fun onConfigurationChanged(newConfig: Configuration) {
+//        super.finish()
+//        super.onConfigurationChanged(newConfig)
+//    }
+//
+//    override fun getLayout(): Int = R.layout.activity_main
+//
+//    override fun initialSetUp() {
+//        DownloadTask().start()
+//    }
+//
+//    override fun setUIData() {
+//
+//    }
+//
+//    override fun setUIListener() {
+//
+//    }
 
-    }
-
-    override fun setUIData() {
-
-    }
-
-    override fun setUIListener() {
-
-    }
+//    private class DownloadTask : Thread() {
+//        override fun run() {
+//            SystemClock.sleep(2000 * 10.toLong())
+//        }
+//    }
 }
